9b9949e33064ee41b59de19a41ed7a1f9ac90c7f
HarshaD2023 <148148941+HarshaD2023@users.noreply.github.com>
2023-10-16 14:46:12 -0500

    Update streamlit_app.py
    added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit
 streamlit.title('Today is Monday')
 
 streamlit.title('Winter is here')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
